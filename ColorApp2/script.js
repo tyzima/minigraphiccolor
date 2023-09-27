2b3d10ca6a96c420f04717dc35b79fbd5a51ab0a
tyzima <41972512+tyzima@users.noreply.github.com>
2023-09-27 02:51:40 -0400

    Update script.js

--- ColorApp2/script.js
+++ ColorApp2/script.js
@@ -3,17 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const resultDiv = document.getElementById('result');
     const logoCanvas = document.getElementById('logoCanvas');
     const logoUpload = document.getElementById('logoUpload');
+    const dropArea = document.getElementById('dropArea');
     const ctx = logoCanvas.getContext('2d');
     
     let pantoneData = [];
-
-    // Your company colors
-    const companyColors = [
-        { name: 'White', hex: '#FFFFFF' },
-        { name: 'Scarlet Red', hex: '#c10230' },
-        // ... add all your other colors here
-        { name: 'Mint', hex: '#8CE2D0' }
-    ];
 
     // Fetch Pantone data from JSON
     fetch('./pantone_CMYK_RGB_Hex.json')
@@ -22,17 +15,15 @@ document.addEventListener("DOMContentLoaded", function () {
             pantoneData = data;
         });
 
-    // Manual color search
     searchBtn.addEventListener('click', function () {
         const hexInput = document.getElementById('hexInput').value;
         const closestPantone = findClosestPantone(hexInput);
-        const closestCompanyColor = findClosestCompanyColor(hexInput);
-        displayResult(closestPantone, closestCompanyColor);
+        displayResult(closestPantone);
     });
 
-    // Drag-and-drop and click-to-upload functionalities
+    // Drag and drop for the drop area
     ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-        logoCanvas.addEventListener(eventName, preventDefaults, false);
+        dropArea.addEventListener(eventName, preventDefaults, false);
     });
 
     function preventDefaults(e) {
@@ -40,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
         e.stopPropagation();
     }
 
-    logoCanvas.addEventListener('drop', handleDrop, false);
+    dropArea.addEventListener('drop', handleDrop, false);
 
     function handleDrop(e) {
         const dt = e.dataTransfer;
@@ -48,46 +39,36 @@ document.addEventListener("DOMContentLoaded", function () {
         uploadImage(file);
     }
 
-    logoCanvas.addEventListener('click', function () {
-        logoUpload.click();
-    });
-
     logoUpload.addEventListener('change', function () {
         uploadImage(this.files[0]);
     });
 
     function uploadImage(file) {
         const reader = new FileReader();
-
         reader.onload = function (e) {
             const img = new Image();
             img.src = e.target.result;
 
             img.onload = function () {
-                const scale = Math.min(250 / img.width, 250 / img.height);
-                logoCanvas.width = img.width * scale;
-                logoCanvas.height = img.height * scale;
-                ctx.clearRect(0, 0, logoCanvas.width, logoCanvas.height);
-                ctx.drawImage(img, 0, 0, img.width * scale, img.height * scale);
+                logoCanvas.width = img.width;
+                logoCanvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
             };
         };
 
         reader.readAsDataURL(file);
     }
 
-    // Color picker functionality
+    // Color picker
     logoCanvas.addEventListener('mousemove', function (e) {
         const x = e.clientX - logoCanvas.getBoundingClientRect().left;
         const y = e.clientY - logoCanvas.getBoundingClientRect().top;
         const pixel = ctx.getImageData(x, y, 1, 1).data;
         const hex = "#" + ("000000" + rgbToHex(pixel[0], pixel[1], pixel[2])).slice(-6);
-        
-        const closestPantone = findClosestPantone(hex);
-        const closestCompanyColor = findClosestCompanyColor(hex);
-        displayResult(closestPantone, closestCompanyColor);
-    });
 
-    // ... (Your existing functions for finding closest Pantone, converting HEX to RGB, etc.)
+        const closestPantone = findClosestPantone(hex);
+        displayResult(closestPantone);
+    });
 
     function findClosestPantone(hex) {
         let closestDistance = Infinity;
@@ -101,20 +82,6 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
         return closestPantone;
-    }
-
-    function findClosestCompanyColor(hex) {
-        let closestDistance = Infinity;
-        let closestCompanyColor = null;
-
-        for (const color of companyColors) {
-            const distance = colorDistance(hexToRgb(hex), hexToRgb(color.hex));
-            if (distance < closestDistance) {
-                closestDistance = distance;
-                closestCompanyColor = color;
-            }
-        }
-        return closestCompanyColor;
     }
 
     function hexToRgb(hex) {
@@ -134,16 +101,18 @@ document.addEventListener("DOMContentLoaded", function () {
         );
     }
 
-    function displayResult(pantone, closestCompanyColor) {
+    function displayResult(pantone) {
         const imgSrc = `https://www.pantone.com/media/wysiwyg/color-finder/img/pantone-color-chip-${pantone.Code.replace(' ', '-').toLowerCase()}-c.webp`;
         resultDiv.innerHTML = `
             <h2>Closest Pantone Color</h2>
             <div>
                 <img src="${imgSrc}" alt="${pantone.Code}" style="width: 80px;">
-                <div class="company-color-label" style="background-color: ${closestCompanyColor.hex};">
-                    Closest Company Color: ${closestCompanyColor.name}
-                </div>
-                <!-- ... rest of your existing code ... -->
+            </div>
+            <div>
+                <p><strong>Code:</strong> ${pantone.Code}</p>
+                <p><strong>HEX:</strong> ${pantone.Hex}</p>
+                <p><strong>RGB:</strong> (${pantone.R}, ${pantone.G}, ${pantone.B})</p>
+                <p><strong>CMYK:</strong> C${pantone.C} M${pantone.M} Y${pantone.Y} K${pantone.K}</p>
             </div>
         `;
     }
