38352c60d784d58f5cf201ccfa14843a22e1b72c
tyzima <41972512+tyzima@users.noreply.github.com>
2023-09-27 18:44:55 -0400

    Update script.js

--- Colors/script.js
+++ Colors/script.js
@@ -233,6 +233,3 @@ function cie76(color1, color2){
 document.getElementById('filterPrintability').addEventListener('change', applyFilters);
 document.getElementById('filterBrands').addEventListener('change', applyFilters);
 document.getElementById('filterColor').addEventListener('change', applyFilters);
-
-
-
