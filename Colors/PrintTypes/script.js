988dce5536503a7069d2e27d453fbea2cf5c2365
tyzima <41972512+tyzima@users.noreply.github.com>
2023-09-29 00:34:38 -0400

    new

---
+++ Colors/PrintTypes/script.js
@@ -0,0 +1,10 @@
+function showLightbox(id) {
+    const lightbox = document.getElementById(id);
+    lightbox.style.display = 'block';
+  }
+  
+  function hideLightbox(id) {
+    const lightbox = document.getElementById(id);
+    lightbox.style.display = 'none';
+  }
+  
