e13265f9dca3c749a4345ddeb56832351846097c
tyzima <41972512+tyzima@users.noreply.github.com>
2023-09-29 13:00:52 -0400

    Update fetchtoAirtable.js

--- netlify/functions/fetchtoAirtable.js
+++ netlify/functions/fetchtoAirtable.js
@@ -1,21 +1,38 @@
 var Airtable = require('airtable');
 
 exports.handler = async function(event, context) {
-  var base = new Airtable({apiKey: process.env.AIRTABLE_ACCESS_TOKEN}).base('appVZNtd4jkDSXdkN');
-  let records = [];
+  try {
+    var base = new Airtable({apiKey: process.env.AIRTABLE_ACCESS_TOKEN}).base('appVZNtd4jkDSXdkN');
+    let records = [];
 
-  await base('Logos').select({
-    maxRecords: 3,
-    view: "Not Uploaded"
-  }).eachPage(function page(recs, fetchNextPage) {
-    records = [...records, ...recs];
-    fetchNextPage();
-  }, function done(err) {
-    if (err) { console.error(err); return; }
-  });
+    await new Promise((resolve, reject) => {
+      base('Logos').select({
+        maxRecords: 3,
+        view: "Not Uploaded"
+      }).eachPage(function page(recs, fetchNextPage) {
+        records = [...records, ...recs];
+        fetchNextPage();
+      }, function done(err) {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(records)
-  };
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(records),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'An error occurred' }),
+    };
+  }
 };
