79e7dad7307d0fa9ce307d5f5e768c4ef6347c65
tyzima <41972512+tyzima@users.noreply.github.com>
2023-10-01 00:51:58 -0400

    New

--- DTFApp/script.js
+++ DTFApp/script.js
@@ -1,15 +1,27 @@
-let logos = []; // Array to store logo objects
-let dragging = false; // Are we dragging?
-let scaling = false; // Are we scaling?
-let dragOffset = { x: 0, y: 0 };
-let selectedLogo = null; // Currently selected logo
+const canvas = document.getElementById("logoCanvas");
+const ctx = canvas.getContext("2d");
 
-// Function to set canvas size
+// Initialize variables
+let logos = [];
+let dragging = false;
+let scaling = false;
+let dragOffset = { x: 0, y: 0 };
+let selectedLogo = null;
+
+const sizes = {
+  small: { width: 11 * 20, height: 12.5 * 20 },
+  medium: { width: 22.5 * 20, height: 12.5 * 20 },
+  large: { width: 22.5 * 20, height: 25 * 20 },
+  xlarge: { width: 22.5 * 20, height: 60 * 20 },
+};
+
+// Set the initial canvas size
+setSize('small');
+
 function setSize(size) {
-  currentSize = sizes[size];
-  canvas.width = currentSize.width;
-  canvas.height = currentSize.height;
-  
+  const newSize = sizes[size];
+  canvas.width = newSize.width;
+  canvas.height = newSize.height;
   if (size === 'xlarge') {
     canvas.style.width = '100%';
     canvas.style.height = 'auto';
@@ -20,38 +32,33 @@ function setSize(size) {
   render();
 }
 
-// Function to add logos to sheet
 async function addToSheet() {
-    const logoID = document.getElementById("logoID").value;
-    const qty = parseInt(document.getElementById("qty").value);
-    const width = parseInt(document.getElementById("width").value);
-    
-    // Assuming the image is hosted publicly
-    const imgUrl = `https://res.cloudinary.com/laxdotcom/image/upload/v1663810522/0012I00002myQeQ/SVG.0012I00002myQeQ.${logoID}.svg`;
-    
-    const img = new Image();
-    img.crossOrigin = "anonymous";  // This line is important to avoid security issues
-    
-    img.onload = function() {
-      let x = 0, y = 0;
-      for (let i = 0; i < qty; i++) {
-        ctx.drawImage(img, x, y, width, (width * img.height) / img.width);
-        
-        x += width + 5; // 5 units for spacing
-        if (x + width > canvas.width) {
-          x = 0;
-          y += (width * img.height) / img.width + 5;
-        }
-      }
-    };
-    
-    img.src = imgUrl;
- 
-  logos.push({ img, x: 0, y: 0, width, height: (width * img.height) / img.width });
-  render();
+  const logoID = document.getElementById("logoID").value;
+  const qty = parseInt(document.getElementById("qty").value);
+  const width = parseInt(document.getElementById("width").value);
+
+  const imgUrl = `https://res.cloudinary.com/laxdotcom/image/upload/logos/${logoID}.svg`;
+
+  const img = new Image();
+  img.crossOrigin = "anonymous";
+
+  img.onload = function() {
+    for (let i = 0; i < qty; i++) {
+      const logo = {
+        img: img,
+        x: 0,
+        y: 0,
+        width: width,
+        height: (width * img.height) / img.width
+      };
+      logos.push(logo);
+    }
+    render();
+  };
+
+  img.src = imgUrl;
 }
 
-// Function to render all logos
 function render() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   logos.forEach(logo => {
@@ -59,12 +66,10 @@ function render() {
   });
 }
 
-// Function to check if a point is inside a logo
 function isInsideLogo(x, y, logo) {
   return x > logo.x && x < logo.x + logo.width && y > logo.y && y < logo.y + logo.height;
 }
 
-// Mouse down event
 canvas.addEventListener('mousedown', function(e) {
   const rect = canvas.getBoundingClientRect();
   const x = e.clientX - rect.left;
@@ -78,7 +83,6 @@ canvas.addEventListener('mousedown', function(e) {
   }
 });
 
-// Mouse move event
 canvas.addEventListener('mousemove', function(e) {
   if (dragging && selectedLogo) {
     const rect = canvas.getBoundingClientRect();
@@ -92,12 +96,10 @@ canvas.addEventListener('mousemove', function(e) {
   }
 });
 
-// Mouse up event
 canvas.addEventListener('mouseup', function(e) {
   dragging = false;
 });
 
-// Keyboard events for delete, copy, and paste
 document.addEventListener('keydown', function(e) {
   if (selectedLogo) {
     if (e.key === 'Delete') {
@@ -107,33 +109,19 @@ document.addEventListener('keydown', function(e) {
       }
       render();
     } else if (e.key === 'c' && e.ctrlKey) {
-      // Ctrl+C to copy
       logos.push({ ...selectedLogo });
       render();
     } else if (e.key === 'v' && e.ctrlKey) {
-      // Ctrl+V to paste
-      if (selectedLogo) {
-        logos.push({ ...selectedLogo, x: selectedLogo.x + 10, y: selectedLogo.y + 10 });
-        render();
-      }
+      logos.push({ ...selectedLogo, x: selectedLogo.x + 10, y: selectedLogo.y + 10 });
+      render();
     }
   }
 });
 
-// Function to add logos to sheet
-
-
-// Function to download as PDF
 function downloadPDF() {
-  const pdf = new jsPDF();
-  pdf.addImage(canvas.toDataURL("image/png"), "PNG", 0, 0);
-  pdf.save("logos.pdf");
+  // We'll tackle this part later
 }
 
-// Function to download as PNG
 function downloadPNG() {
-  const link = document.createElement('a');
-  link.download = 'logos.png';
-  link.href = canvas.toDataURL('image/png');
-  link.click();
+  // We'll tackle this part later
 }
