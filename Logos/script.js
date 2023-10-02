42eebc98656f40bc777b49b805678c34574a021d
tyzima <41972512+tyzima@users.noreply.github.com>
2023-10-02 02:51:37 -0400

    NEwScript

--- Logos/script.js
+++ Logos/script.js
@@ -1,102 +1,72 @@
-// Fetching both JSON files
-Promise.all([
-    fetch('logos.json').then(res => res.json()),
-    fetch('colors.json').then(res => res.json())
-  ]).then(([logoData, colorData]) => {
-    const logoGrid = document.getElementById('logoGrid');
-    const searchTeamName = document.getElementById('searchTeamName');
-    const searchDescription = document.getElementById('searchDescription');
-    const searchLogoID = document.getElementById('searchLogoID');
-    const changeBgColorBtn = document.getElementById('changeBgColor');
+// Initialize the app
+function initApp(logos) {
+    const logoGrid = document.getElementById('logo-grid');
     
-    let currentPage = 1;
-    const recordsPerPage = 10;
+    logos.forEach(logo => {
+      const logoCard = document.createElement('div');
+      logoCard.classList.add('logo-card');
   
-    // Function to create logo cards
-    const createLogoCards = (logos, page = 1) => {
-      // Clear the grid first
-      logoGrid.innerHTML = '';
+      // Display Logo
+      const logoImg = document.createElement('img');
+      logoImg.src = logo.PNG;
+      logoImg.style.maxWidth = '300px';
+      logoCard.appendChild(logoImg);
   
-      // Calculate starting and end records
-      const start = (page - 1) * recordsPerPage;
-      const end = start + recordsPerPage;
+      // Display Description
+      const desc = document.createElement('p');
+      desc.style.fontSize = '8px';
+      desc.style.color = 'lightgrey';
+      desc.textContent = logo.Description.toUpperCase();
+      logoCard.appendChild(desc);
   
-      // Paginate data
-      const paginatedLogos = logos.slice(start, end);
+      // Display Logo ID
+      const logoID = document.createElement('div');
+      logoID.textContent = `Logo ID: ${logo['Logo ID']}`;
+      logoID.style.borderRadius = '50%';
+      logoID.style.position = 'absolute';
+      logoID.style.top = '10px';
+      logoID.style.left = '10px';
+      logoCard.appendChild(logoID);
   
-      paginatedLogos.forEach(logo => {
-        const logoCard = document.createElement('div');
-        logoCard.className = 'logo-card';
-        
-        const logoIDCircle = document.createElement('div');
-        logoIDCircle.innerText = logo['Logo ID'];
-        logoIDCircle.className = 'logo-id-circle';
-        
-        const logoImage = document.createElement('img');
-        logoImage.src = logo['PNG'];
-        logoImage.className = 'logo-image';
-        
-        const description = document.createElement('div');
-        description.innerText = logo['Description'].toUpperCase();
-        description.className = 'logo-description';
-        
-        const teamName = document.createElement('div');
-        teamName.innerText = logo['Account Name'];
-        teamName.className = 'team-name';
-        
-        const editColorsBtn = document.createElement('button');
-        editColorsBtn.innerText = 'Edit Colors';
-        editColorsBtn.onclick = () => window.open(logo['LaxInkEditor'], '_blank');
-        
-        const colorHelper = document.createElement('div');
-        colorHelper.className = 'color-helper';
-        
-        // Populate color swatches using colorData
-        const colorSwatch = document.createElement('div');
-        colorSwatch.className = 'color-swatch';
-        colorSwatch.style.backgroundColor = colorData[logo['ColorHelper'].substring(1)]['HexCode'];
-        colorHelper.appendChild(colorSwatch);
+      // Display VariationOf
+      if (logo.VariationOf) {
+        const variationOf = document.createElement('p');
+        variationOf.textContent = `Variation of: ${logo.VariationOf}`;
+        logoCard.appendChild(variationOf);
+      }
   
-        logoCard.appendChild(logoIDCircle);
-        logoCard.appendChild(logoImage);
-        logoCard.appendChild(description);
-        logoCard.appendChild(teamName);
-        logoCard.appendChild(editColorsBtn);
-        logoCard.appendChild(colorHelper);
-        
-        logoGrid.appendChild(logoCard);
-      });
-    };
+      // Display Team Name
+      const teamName = document.createElement('p');
+      teamName.style.fontSize = '12px';
+      teamName.textContent = `Team Name: ${logo['Account Name']}`;
+      logoCard.appendChild(teamName);
   
-    // Initialize with all logos
-    createLogoCards(logoData, currentPage);
+      // Display Edit Colors Button
+      const editColorsBtn = document.createElement('a');
+      editColorsBtn.textContent = 'Edit Colors';
+      editColorsBtn.href = logo.LaxInkEditor;
+      editColorsBtn.target = '_blank';
+      logoCard.appendChild(editColorsBtn);
   
-    // Add search functionality by Team Name
-    searchTeamName.addEventListener('input', function() {
-      const filteredLogos = logoData.filter(logo => 
-        logo['Account Name'].toLowerCase().includes(this.value.toLowerCase())
-      );
-      createLogoCards(filteredLogos, currentPage);
+      // Display Color Helper
+      const colorHelper = document.createElement('div');
+      const colorSwatch = document.createElement('div');
+      colorSwatch.style.width = '10px';
+      colorSwatch.style.height = '10px';
+      colorSwatch.style.borderRadius = '50%';
+      colorSwatch.style.backgroundColor = logo.ColorHelper;
+      colorHelper.appendChild(colorSwatch);
+      logoCard.appendChild(colorHelper);
+  
+      logoGrid.appendChild(logoCard);
     });
+  }
   
-    // Add search functionality by Description
-    searchDescription.addEventListener('input', function() {
-      const filteredLogos = logoData.filter(logo => 
-        logo['Description'].toLowerCase().includes(this.value.toLowerCase())
-      );
-      createLogoCards(filteredLogos, currentPage);
-    });
+  // Initialize the color picker for background colors
+  function initColorPicker() {
+    const colorPicker = document.getElementById('color-picker');
   
-    // Add search functionality by Logo ID
-    searchLogoID.addEventListener('input', function() {
-      const filteredLogos = logoData.filter(logo => 
-        logo['Logo ID'].toString().includes(this.value)
-      );
-      createLogoCards(filteredLogos, currentPage);
-    });
-  
-    // Change background color
-    const colorOptions = [
+    const allowedColors = [
       "#c10230", "#85152B", "#7a303f", "#8a2432", "#c2502e", "#f13200",
       "#fde021", "#ffb71b", "#d0b787", "#124734", "#93d500", "#007934",
       "#85DFFF", "#79a3dc", "#6aaae4", "#004a98", "#002DAD", "#003595",
@@ -104,10 +74,53 @@ Promise.all([
       "#333F48", "#373a36", "#6E6259", "#F1E6B2", "#4c372b", "#F99FC9",
       "#E31C79", "#cc5599", "#e3e829", "#005d70"
     ];
-    
-    changeBgColorBtn.addEventListener('click', function() {
-      const randomIndex = Math.floor(Math.random() * colorOptions.length);
-      document.body.style.backgroundColor = colorOptions[randomIndex];
+  
+    allowedColors.forEach(hexCode => {
+      const colorSwatch = document.createElement('div');
+      colorSwatch.style.width = '20px';
+      colorSwatch.style.height = '20px';
+      colorSwatch.style.borderRadius = '50%';
+      colorSwatch.style.backgroundColor = hexCode;
+      colorSwatch.addEventListener('click', () => {
+        document.body.style.backgroundColor = hexCode;
+      });
+  
+      colorPicker.appendChild(colorSwatch);
+    });
+  }
+  
+  // Fetch the logo data
+  fetch('./Logos.JSON')
+    .then(response => response.json())
+    .then(data => {
+      // Initialize the app with logo data
+      initApp(data);
+    });
+  
+  // Initialize the color picker with allowed background colors
+  initColorPicker();
+  
+  // Search Functionality
+  const searchBox = document.getElementById('search-box');
+  
+  searchBox.addEventListener('input', (e) => {
+    const query = e.target.value.toLowerCase();
+    const logoCards = document.querySelectorAll('.logo-card');
+  
+    logoCards.forEach(card => {
+      const teamName = card.querySelector('p').textContent.toLowerCase();
+      const description = card.querySelector('img').alt.toLowerCase();
+      const logoID = card.querySelector('div').textContent.split(': ')[1];
+  
+      if (
+        teamName.includes(query) ||
+        description.includes(query) ||
+        logoID.includes(query)
+      ) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
     });
   });
   
