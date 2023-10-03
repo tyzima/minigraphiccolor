eeea0c8e6f09aa267cd56beae393c303ebebf31c
tyzima <41972512+tyzima@users.noreply.github.com>
2023-10-03 00:25:58 -0400

    Send

--- Buffer/script.js
+++ Buffer/script.js
@@ -1,10 +1,21 @@
-document.getElementById("enter-button").addEventListener("click", function() {
+// Function to check password and redirect
+function checkPasswordAndRedirect() {
     const input = document.getElementById("password-input").value;
   
-    if (input === "norwalk") {
+    if (input === "logo3s") {
       window.location.href = "http://www.lax.ink/logos";
     } else {
       alert("Wrong password!");
+    }
+  }
+  
+  // Event listener for click on the "enter" button
+  document.getElementById("enter-button").addEventListener("click", checkPasswordAndRedirect);
+  
+  // Event listener for "Enter" key
+  document.getElementById("password-input").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+      checkPasswordAndRedirect();
     }
   });
   
