7fdb12db3192d540a0dd2367a827841888327972
tyzima <41972512+tyzima@users.noreply.github.com>
2023-10-03 13:23:31 -0400

    simple

--- Logos/LLB/script.js
+++ Logos/LLB/script.js
@@ -1,79 +1,42 @@
-let logos = [];
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("../logos.json")
+    .then(response => response.json())
+    .then(data => {
+      const logoContainer = document.getElementById("logo-container");
+      const logoSelect = document.getElementById("logo-select");
 
-async function fetchLogos() {
-  const response = await fetch('../logos.json');
-  if (response.ok) {
-    const data = await response.json();
-    return data;
-  } else {
-    console.error('Failed to fetch logos:', response.status, response.statusText);
-    return [];
-  }
-}
+      data.forEach(logo => {
+        // Populate logo cards
+        const logoCard = document.createElement("div");
+        logoCard.className = "logo-card";
+        const logoImage = document.createElement("img");
+        logoImage.className = "logo-image";
+        logoImage.src = logo.PNG;
+        logoCard.appendChild(logoImage);
+        logoContainer.appendChild(logoCard);
 
-async function initialize() {
-  logos = await fetchLogos();
-  populateLogoCards();
-  populateDropdown();
-}
-
-function populateLogoCards() {
-  const logoContainer = document.getElementById('logo-container');
-  logos.forEach((logo) => {
-    const logoCard = document.createElement('div');
-    logoCard.className = 'logo-card';
-  
-    const logoNumber = document.createElement('p');
-    logoNumber.innerText = `Logo ID: ${logo['Logo ID']}`;
-  
-    const logoImage = document.createElement('img');
-    logoImage.className = 'logo-image';
-    logoImage.src = logo['PNG'];
-  
-    logoCard.appendChild(logoNumber);
-    logoCard.appendChild(logoImage);
-    logoContainer.appendChild(logoCard);
-  });
-}
-
-function populateDropdown() {
-  const logoSelect = document.getElementById('logo-select');
-  logos.forEach((logo) => {
-    const option = document.createElement('option');
-    option.value = logo['Logo ID'];
-    option.innerText = `${logo['Description']} (${logo['Logo ID']})`;
-    logoSelect.appendChild(option);
-  });
-}
-
-document.getElementById('generate-pdf').addEventListener('click', async () => {
-  const { jsPDF } = window.jspdf;
-  const pdf = new jsPDF();
-  
-  const selectedLogos = Array.from(document.getElementById('logo-select').selectedOptions).map(option => option.value);
-  
-  let y = 10;
-  for (const id of selectedLogos) {
-    const logo = logos.find(logo => logo['Logo ID'] === parseInt(id));
-    pdf.text(`Logo ID: ${logo['Logo ID']}`, 10, y);
-
-    const imgData = await fetch(logo['PNG']).then(r => r.blob()).then(blob => {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.onerror = reject;
-        reader.readAsDataURL(blob);
+        // Populate dropdown
+        const option = document.createElement("option");
+        option.value = logo["Logo ID"];
+        option.textContent = `${logo.Description} (${logo["Logo ID"]})`;
+        logoSelect.appendChild(option);
       });
-    });
 
-    pdf.addImage(imgData, 'PNG', 10, y + 10, 150, 150);
+      // PDF generation
+      document.getElementById("generate-pdf").addEventListener("click", () => {
+        const { jsPDF } = window.jspdf;
+        const pdf = new jsPDF();
+        const selectedLogos = Array.from(logoSelect.selectedOptions).map(option => option.value);
 
-    y += 170;
-  }
-  
-  pdf.save('logos.pdf');
+        let y = 10;
+        selectedLogos.forEach(id => {
+          const selectedLogo = data.find(logo => logo["Logo ID"] === parseInt(id));
+          pdf.text(`Logo ID: ${selectedLogo["Logo ID"]}`, 10, y);
+          y += 20;  // Move down for the next logo
+        });
+
+        pdf.save("logos.pdf");
+      });
+    })
+    .catch(error => console.error("Error fetching logos:", error));
 });
-
-$('#logo-select').select2();
-
-initialize();
