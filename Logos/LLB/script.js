ad17029e6b12f4d9f0256a955f4c62c5df9edb98
tyzima <41972512+tyzima@users.noreply.github.com>
2023-10-03 14:32:22 -0400

    MM

--- Logos/LLB/script.js
+++ Logos/LLB/script.js
@@ -1,3 +1,100 @@
+// Fetch logos data from '../logos.json'
+fetch('../logos.json')
+  .then(response => response.json())
+  .then(data => populateDropdown(data));
+
+// Populate dropdown
+function populateDropdown(logosData) {
+  const dropdown = document.getElementById('logoDropdown');
+  const selectedLogosDiv = document.getElementById('selectedLogos');
+  const choices = new Choices(dropdown, {
+    removeItemButton: true,
+    duplicateItemsAllowed: false,
+  });
+
+  const logoChoices = logosData.map((logo, index) => ({
+    value: index,
+    label: `${logo['Logo ID']} - ${logo.Description}`,
+    selected: false,
+    disabled: false,
+  }));
+
+  choices.setChoices(logoChoices, 'value', 'label', false);
+
+  // Update selected logos display
+  dropdown.addEventListener('choice', function(event) {
+    const logo = logosData[event.detail.choice.value];
+    const img = document.createElement('img');
+    img.src = logo['PNG'];
+    img.height = 50;
+    selectedLogosDiv.appendChild(img);
+  });
+
+  // Save PDF
+  document.getElementById('savePDF').addEventListener('click', async function() {
+    const selectedIndices = choices.getValue(true).map(value => parseInt(value));
+    const selectedLogos = selectedIndices.map(index => logosData[index]);
+    const teamName = document.getElementById('teamName').value.toUpperCase();
+    
+    // Create PDF
+    const { jsPDF } = window.jspdf;
+    const pdf = new jsPDF();
+    
+    pdf.setFontSize(22);
+    pdf.text(teamName, 10, 10);
+    
+    const imagePromises = selectedLogos.map((logo, i) => {
+      return new Promise(async (resolve) => {
+        const pageIndex = Math.floor(i / 9);
+        const x = 20 + (i % 3) * 60;
+        const y = 40 + (Math.floor(i / 3) % 3) * 60;
+        
+        if (i % 9 === 0 && i !== 0) {
+          pdf.addPage();
+          pdf.setFontSize(22);
+          pdf.text(teamName, 10, 10);
+        }
+        
+        // Draw a light grey rectangle with rounded corners as the background
+        pdf.setFillColor(200, 200, 200); // light grey
+        pdf.roundedRect(x - 2, y - 12, 54, 54, 3, 3, 'F');
+        
+        // Draw pill-shaped container for Logo ID
+        pdf.setFillColor(255, 255, 255); // white
+        pdf.roundedRect(x, y - 20, 30, 10, 5, 5, 'F');
+        
+        pdf.setFontSize(12);
+        pdf.text(`${logo['Logo ID']}`, x + 5, y - 12);
+        
+        // Fetch the image and add it to the PDF
+        const img = new Image();
+        img.src = logo['PNG'];
+        img.onload = async function() {
+          const aspectRatio = img.width / img.height;
+          const fixedWidth = 50;
+          const calculatedHeight = fixedWidth / aspectRatio;
+          
+          const centerX = x + (fixedWidth - calculatedWidth) / 2;
+          const centerY = y + (fixedWidth - calculatedHeight) / 2;
+          
+          const imgBlob = await fetch(logo['PNG']).then(r => r.blob());
+          const reader = new FileReader();
+          reader.readAsDataURL(imgBlob);
+          reader.onloadend = function() {
+            const base64data = reader.result;
+            pdf.addImage(base64data, 'PNG', centerX, centerY, fixedWidth, calculatedHeight);
+            resolve();
+          };
+        };
+      });
+    });
+    
+    await Promise.all(imagePromises);
+    pdf.save(`${teamName}.pdf`);
+  });
+}
+
+
 
 // Fetch logos data from '../logos.json'
 fetch('../logos.json')
@@ -80,8 +177,8 @@ function populateDropdown(logosData) {
           const fixedWidth = 50;
           const calculatedHeight = fixedWidth / aspectRatio;
           
-          const centerX = x + (fixedWidth - calculatedWidth) / 2;
-          const centerY = y + (fixedWidth - calculatedHeight) / 2;
+          const centerX = x + (fixedWidth - fixedWidth) / 2;  // fixedWidth is used here for centerX calculation
+          const centerY = y + (54 - calculatedHeight) / 2;  // 54 is the height of the container
           
           const imgBlob = await fetch(logo['PNG']).then(r => r.blob());
           const reader = new FileReader();
@@ -92,6 +189,9 @@ function populateDropdown(logosData) {
             resolve();
           };
         };
+
+
+
       });
     });
     
