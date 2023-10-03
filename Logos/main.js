92f71c2dd00befa92018d7f876fbfe4727f17e3b
tyzima <41972512+tyzima@users.noreply.github.com>
2023-10-02 20:49:25 -0400

    Test

--- Logos/main.js
+++ Logos/main.js
@@ -120,12 +120,15 @@ logoCard.appendChild(teamName);
     logoCard.appendChild(logoImg);
 
 
-    // Display Edit Colors Button
-    const editColorsBtn = document.createElement('a');
-    editColorsBtn.innerHTML = '<i class="material-icons">palette</i>';
-    editColorsBtn.href = logo.LaxInkEditor;
-    editColorsBtn.target = '_blank';
-    logoCard.appendChild(editColorsBtn);
+ // Display Edit Colors Button only if hideSearch is not 'true'
+if (hideSearch !== 'true') {
+  const editColorsBtn = document.createElement('a');
+  editColorsBtn.innerHTML = '<i class="material-icons">palette</i>';
+  editColorsBtn.href = logo.LaxInkEditor;
+  editColorsBtn.target = '_blank';
+  logoCard.appendChild(editColorsBtn);
+}
+
 
 
 
